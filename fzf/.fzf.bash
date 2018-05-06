c2fac91bf305f8f2cb6156dba0f45584120ca2c1
Daniel <danieltorres1987@gmail.com>
2018-05-06 03:11:38 -0700

    updates

--- fzf/.fzf.bash
+++ fzf/.fzf.bash
@@ -1,14 +1,14 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/daniel/.fzf/bin* ]]; then
-  export PATH="$PATH:/home/daniel/.fzf/bin"
+if [[ ! "$PATH" == */home/daniel/.fzf/fzf/bin* ]]; then
+  export PATH="$PATH:/home/daniel/.fzf/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/daniel/.fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "/home/daniel/.fzf/fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/daniel/.fzf/shell/key-bindings.bash"
+source "/home/daniel/.fzf/fzf/shell/key-bindings.bash"
 
